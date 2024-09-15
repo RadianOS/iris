89e4e7c75f6d64b92bd0e664f1d2d590a3ece899
Rudy ツ <msharmion@gmail.com>
2024-09-15 20:11:13 +0530

    Update copyrights

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,15 @@ use util::cmd_handler;
 const VERSION_TEXT: &str = r#"
 d8b       d8b                    Iris Package Manager         
 Y8P       Y8P                 -------------------------
-                      
-888888d888888.d8888b          Iris v1.0.0
-888888P"  88888K              Copyright (C) 2024-2025 RadianOS Development Team
+                              Iris v1.0.0
+888888d888888.d8888b          Copyright (C) 2024-2025 RadianOS Development Team
+888888P"  88888K              Copyright (C) 2024-2025 Rainbow Linux Development Team
 888888    888"Y8888b.         Copyright (C) 2024-2025 Atiksh Sharma
-888888    888     X88         This program may be freely redistributed under
-888888    888 88888P'          the terms of the GNU General Public License.
+888888    888     X88         Copyright (C) 2024-2025 Kıvılcım İ. Öztürk
+888888    888 88888P'         
+                              
+                              This program may be freely redistributed under
+                                the terms of the GNU General Public License.
 "#;
 
 fn print_version() {
