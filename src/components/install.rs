7bdef1386e124e0a1a96e42e478e7285ddf519ea
rudy-in <msharmion@gmail.com>
2024-09-18 09:36:57 +0530

    Seperate install logic , Update Copyright

---
+++ src/components/install.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::{self,Result as IoResult};
+use reqwest::blocking::get;
+use tar::Archive;
+use xz2::read::XzDecoder;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::io::Read;
+use crate::confirm;
+
+pub fn download_package(url: &str, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = get(url)?;
+    let mut file = File::create(output_path)?;
+    io::copy(&mut response.take(usize::MAX as u64), &mut file)?;
+    Ok(())
+}
+
+pub fn extract_package(tar_xz_path: &str, output_dir: &str) -> IoResult<()> {
+    let file = File::open(tar_xz_path)?;
+    let decompressor = XzDecoder::new(file);
+    let mut archive = Archive::new(decompressor);
+    archive.unpack(output_dir)?;
+    Ok(())
+}
+
+pub fn install_packages(pkgs: &[String], force: bool) {
+    if !force && !confirm("Are you sure you want to install the packages") {
+        println!("Installation aborted.");
+        return;
+    }
+    
+    let total = pkgs.len() as u64;
+    let pb = ProgressBar::new(total);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] {wide_bar} {pos}/{len} ({eta})").expect("{}")
+        .progress_chars("#>-"));
+
+    for pkg in pkgs {
+        let url = format!("https://raw.githubusercontent.com/RadianOS/zephpkgs/main/{}", pkg);
+        let output_path = format!("/tmp/{}", pkg);
+
+        if let Err(e) = download_package(&url, &output_path) {
+            eprintln!("Failed to download {}: {}", pkg, e);
+            continue;
+        }
+
+        if let Err(e) = extract_package(&output_path, "/home/rudy") {
+            eprintln!("Failed to extract {}: {}", pkg, e);
+        } else {
+            println!("Successfully installed {}", pkg);
+        }
+
+        pb.inc(1);
+    }
+    pb.finish_with_message("Installation complete");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 pub mod cli;
 pub mod log;
 pub mod util;
-
-use crate::cli::{Cli, Operations};
+pub mod components;
+use crate::components::install;
+use crate::util::cmd_handler;
+use crate::cli::{Cli, Operations}; 
 use colored::Colorize;
 use clap::Parser;
-use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::blocking::get;
-use std::fs::File;
-use std::io::{self, Write, Read, Result as IoResult};
-use tar::Archive;
-use xz2::read::XzDecoder;
-use util::cmd_handler;
+use std::io::{self, Write};
 
 const VERSION_TEXT: &str = r#"
 d8b       d8b                    Iris Package Manager         
@@ -21,7 +17,7 @@ Y8P       Y8P                 -------------------------
 888888P"  88888K              Copyright (C) 2024-2025 Rainbow Linux Development Team
 888888    888"Y8888b.         Copyright (C) 2024-2025 Atiksh Sharma
 888888    888     X88         Copyright (C) 2024-2025 Kıvılcım İ. Öztürk
-888888    888 88888P'         
+888888    888 88888P'         Copyright (C) 2024-2025 Çıt
                               
                               This program may be freely redistributed under
                                 the terms of the GNU General Public License.
@@ -31,7 +27,7 @@ fn print_version() {
     println!("{}", VERSION_TEXT.bright_cyan().bold());
 }
 
-fn confirm(prompt: &str) -> bool {
+pub fn confirm(prompt: &str) -> bool {
     let mut input = String::new();
     
     loop {
@@ -52,53 +48,6 @@ fn confirm(prompt: &str) -> bool {
     }
 }
 
-fn download_package(url: &str, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let response = get(url)?;
-    let mut file = File::create(output_path)?;
-    io::copy(&mut response.take(usize::MAX as u64), &mut file)?;
-    Ok(())
-}
-
-fn extract_package(tar_xz_path: &str, output_dir: &str) -> IoResult<()> {
-    let file = File::open(tar_xz_path)?;
-    let decompressor = XzDecoder::new(file);
-    let mut archive = Archive::new(decompressor);
-    archive.unpack(output_dir)?;
-    Ok(())
-}
-
-fn install_packages(pkgs: &[String], force: bool) {
-    if !force && !confirm("Are you sure you want to install the packages") {
-        println!("Installation aborted.");
-        return;
-    }
-    
-    let total = pkgs.len() as u64;
-    let pb = ProgressBar::new(total);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] {wide_bar} {pos}/{len} ({eta})").expect("REASON")
-        .progress_chars("#>-"));
-
-    for pkg in pkgs {
-        let url = format!("https://raw.githubusercontent.com/RadianOS/zephpkgs/main/{}", pkg);
-        let output_path = format!("/tmp/{}", pkg);
-
-        if let Err(e) = download_package(&url, &output_path) {
-            eprintln!("Failed to download {}: {}", pkg, e);
-            continue;
-        }
-
-        if let Err(e) = extract_package(&output_path, "/home/rudy") {
-            eprintln!("Failed to extract {}: {}", pkg, e);
-        } else {
-            println!("Successfully installed {}", pkg);
-        }
-
-        pb.inc(1);
-    }
-    pb.finish_with_message("Installation complete");
-}
-
 fn main() {
     let cli = Cli::parse();
 
@@ -112,7 +61,7 @@ fn main() {
     if let Some(operation) = cli.operation {
         match operation {
             Operations::Install(install) => {
-                install_packages(&install.pkgs, install.force);
+                install::install_packages(&install.pkgs, install.force);
             }
             Operations::Remove(remove) => {
                 if !remove.force && !remove.yes && !confirm("Are you sure you want to remove the packages") {
